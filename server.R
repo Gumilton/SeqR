e018fda3f349bfdbbe99d347d1fe91f55e218f7b
gumilton <gumilton@gmail.com>
2015-09-02 18:43:03 -0500

    organized

--- server.R
+++ server.R
@@ -7,31 +7,31 @@ shinyServer(function(input, output) {
   
   
   observeEvent(input$dataIn, {
-    source("./dataIn_server.R", local=TRUE)
-    source("./EDA.R")
-    source("./EDA_server.R", local=TRUE)
+    source("./server/dataIn_server.R", local=TRUE)
+    source("./function/EDA.R")
+    source("./server/EDA_server.R", local=TRUE)
     observeEvent(input$controlSet, {
       observeEvent(input$methodSet, {
         if("LIMMA" %in% methods()) {
-          source("./limma.R")
-          source("./limma_server.R", local=TRUE)
+          source("./function/limma.R")
+          source("./server/limma_server.R", local=TRUE)
         }
         
         if("DESeq" %in% methods()) {
-          source("./deseq.R")
-          source("./deseq_server.R", local=TRUE)
+          source("./function/deseq.R")
+          source("./server/deseq_server.R", local=TRUE)
         }
         
         if("DESeq2" %in% methods()) {
-          source("./deseq2.R")
-          source("./deseq2_server.R", local=TRUE)
+          source("./function/deseq2.R")
+          source("./server/deseq2_server.R", local=TRUE)
         }
       })
       
       })
     
     observeEvent(input$showRes, {
-      source("./overlap_server.R", local=TRUE)
+      source("./server/overlap_server.R", local=TRUE)
       
     })
     
